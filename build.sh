f00e9060773b9b13010ae4bde2468110538e2dcf
hypeaches <hypeaches@live.cn>
2019-04-13 00:21:27 +0800

    feat: 实现findcpp

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+tool_name=("findcpp")
+dest_dir="tools"
+script_dir=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+
+tool_dir=$script_dir/$dest_dir
+if [ -d $tool_dir ]
+then
+    rm -rf $tool_dir
+fi
+mkdir $tool_dir
+
+for tool in ${tool_name[@]}
+do
+    cd $script_dir/$tool
+    go build $tool.go
+    if [ -f $tool ]
+    then
+        mv $tool $tool_dir
+    else
+        echo "build failed: $tool"
+    fi
+done
--- findcpp/findcpp.go
+++ findcpp/findcpp.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -10,7 +12,7 @@ import (
 )
 
 type CommandLine struct {
-	input string
+	src string
 	recursive bool
 	quote string
 	lineEnd string
@@ -19,9 +21,11 @@ type CommandLine struct {
 }
 
 var cmd CommandLine = CommandLine{".", false, "", ",", "", []string{}}
+var diffFile map[string]bool = make(map[string]bool)
 
 func main() {
 	initCommandLine()
+	readDiffFile()
 
 	if cmd.recursive {
 		walkDirectoryRecursively()
@@ -31,10 +35,11 @@ func main() {
 }
 
 func initCommandLine() {
-	flag.StringVar(&cmd.input, "input", ".", "搜索目录")
+	flag.StringVar(&cmd.src, "src", ".", "搜索目录")
 	flag.BoolVar(&cmd.recursive, "recursive", true, "是否递归搜索目录下的所有文件")
 	flag.StringVar(&cmd.quote, "quote", "'", "结果用哪种引号包裹，默认单引号")
 	flag.StringVar(&cmd.lineEnd, "line-end", ",", "行尾结束符，默认逗号")
+	flag.StringVar(&cmd.diff, "diff", "", "与指定文件进行比较，找出不同的行并输出")
 
 	var flagSuffix string
 	flag.StringVar(&flagSuffix, "suffix", ".cpp .cc .c", "搜索文件后缀名")
@@ -43,13 +48,39 @@ func initCommandLine() {
 	cmd.suffix = strings.Fields(flagSuffix)
 
 	sep := string(os.PathSeparator)
-	if !strings.HasSuffix(cmd.input, sep) {
-		cmd.input = cmd.input + sep
+	if !strings.HasSuffix(cmd.src, sep) {
+		cmd.src = cmd.src + sep
+	}
+}
+
+func readDiffFile() {
+	if cmd.diff == "" {
+		return;
+	}
+	f, err := os.Open(cmd.diff)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadString('\n')
+
+		line = strings.TrimRight(line, "\n")
+		diffFile[line] = true
+
+		if err == io.EOF {
+			return
+		}
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
 func walkDirectory() {
-	dir, err := ioutil.ReadDir(cmd.input)
+	dir, err := ioutil.ReadDir(cmd.src)
 	if err != nil {
 		panic(err)
 	}
@@ -63,7 +94,10 @@ func walkDirectory() {
 }
 
 func walkDirectoryRecursively() {
-	filepath.Walk(cmd.input, func(filename string, fi os.FileInfo, err error) error {
+	filepath.Walk(cmd.src, func(filename string, fi os.FileInfo, err error) error {
+		if err != nil {
+			panic(err)
+		}
 		if fi.IsDir() {
 			return nil
 		}
@@ -75,8 +109,11 @@ func walkDirectoryRecursively() {
 func outputFileName(filename string) {
 	for _, suffix := range cmd.suffix {
 		if strings.HasSuffix(filename, suffix) {
-			filename = cmd.quote + strings.Replace(filename, cmd.input, "", -1) + cmd.quote + cmd.lineEnd
-			fmt.Println(filename)
+			filename = cmd.quote + strings.Replace(filename, cmd.src, "", -1) + cmd.quote + cmd.lineEnd
+			_, ok := diffFile[filename]
+			if !ok {
+				fmt.Println(filename)
+			}
 		}
 	}
 }
